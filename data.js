fafdb3e6250068f07be892b070380bccf23db0a1
AkkarinRothen <yacris245@gmail.com>
2025-12-04 22:09:42 -0300

    Add kanaDB for Japanese character representations
    This file contains a database of Japanese characters with their Romaji, Hiragana, Katakana representations, groupings, and difficulty levels.

---
+++ data.js
@@ -0,0 +1,18 @@
+// --- BASE DE DATOS DE CARACTERES ---
+// r: Romaji, h: Hiragana, k: Katakana, g: Grupo, l: Nivel de Dificultad (1-3)
+
+const kanaDB = [
+    { r: 'a', h: 'あ', k: 'ア', g: 'row-a', l: 2 }, { r: 'i', h: 'い', k: 'イ', g: 'row-a', l: 1 }, { r: 'u', h: 'う', k: 'ウ', g: 'row-a', l: 1 }, { r: 'e', h: 'え', k: 'エ', g: 'row-a', l: 2 }, { r: 'o', h: 'お', k: 'オ', g: 'row-a', l: 2 },
+    { r: 'ka', h: 'か', k: 'カ', g: 'row-k', l: 2 }, { r: 'ki', h: 'き', k: 'キ', g: 'row-k', l: 2 }, { r: 'ku', h: 'く', k: 'ク', g: 'row-k', l: 1 }, { r: 'ke', h: 'け', k: 'ケ', g: 'row-k', l: 2 }, { r: 'ko', h: 'こ', k: 'コ', g: 'row-k', l: 1 },
+    { r: 'sa', h: 'さ', k: 'サ', g: 'row-s', l: 2 }, { r: 'shi', h: 'し', k: 'シ', g: 'row-s', l: 1 }, { r: 'su', h: 'す', k: 'ス', g: 'row-s', l: 2 }, { r: 'se', h: 'せ', k: 'セ', g: 'row-s', l: 2 }, { r: 'so', h: 'そ', k: 'ソ', g: 'row-s', l: 1 },
+    { r: 'ta', h: 'た', k: 'タ', g: 'row-t', l: 2 }, { r: 'chi', h: 'ち', k: 'チ', g: 'row-t', l: 2 }, { r: 'tsu', h: 'つ', k: 'ツ', g: 'row-t', l: 1 }, { r: 'te', h: 'て', k: 'テ', g: 'row-t', l: 1 }, { r: 'to', h: 'と', k: 'ト', g: 'row-t', l: 1 },
+    { r: 'na', h: 'な', k: 'ナ', g: 'row-n', l: 2 }, { r: 'ni', h: 'に', k: 'ニ', g: 'row-n', l: 2 }, { r: 'nu', h: 'ぬ', k: 'ヌ', g: 'row-n', l: 3 }, { r: 'ne', h: 'ね', k: 'ネ', g: 'row-n', l: 3 }, { r: 'no', h: 'の', k: 'ノ', g: 'row-n', l: 1 },
+    { r: 'ha', h: 'は', k: 'ハ', g: 'row-h', l: 2 }, { r: 'hi', h: 'ひ', k: 'ヒ', g: 'row-h', l: 1 }, { r: 'fu', h: 'ふ', k: 'フ', g: 'row-h', l: 3 }, { r: 'he', h: 'へ', k: 'ヘ', g: 'row-h', l: 1 }, { r: 'ho', h: 'ほ', k: 'ホ', g: 'row-h', l: 2 },
+    { r: 'ma', h: 'ま', k: 'マ', g: 'row-m', l: 2 }, { r: 'mi', h: 'み', k: 'ミ', g: 'row-m', l: 2 }, { r: 'mu', h: 'む', k: 'ム', g: 'row-m', l: 3 }, { r: 'me', h: 'め', k: 'メ', g: 'row-m', l: 2 }, { r: 'mo', h: 'も', k: 'モ', g: 'row-m', l: 2 },
+    { r: 'ya', h: 'や', k: 'ヤ', g: 'row-y', l: 2 }, { r: 'yu', h: 'ゆ', k: 'ユ', g: 'row-y', l: 2 }, { r: 'yo', h: 'よ', k: 'ヨ', g: 'row-y', l: 2 },
+    { r: 'ra', h: 'ら', k: 'ラ', g: 'row-r', l: 1 }, { r: 'ri', h: 'り', k: 'リ', g: 'row-r', l: 1 }, { r: 'ru', h: 'る', k: 'ル', g: 'row-r', l: 2 }, { r: 're', h: 'れ', k: 'レ', g: 'row-r', l: 2 }, { r: 'ro', h: 'ろ', k: 'ロ', g: 'row-r', l: 2 },
+    { r: 'wa', h: 'わ', k: 'ワ', g: 'row-w', l: 2 }, { r: 'wo', h: 'を', k: 'ヲ', g: 'row-w', l: 3 }, { r: 'n', h: 'ん', k: 'ン', g: 'row-w', l: 1 },
+    { r: 'ga', h: 'が', k: 'ガ', g: 'dakuten', l: 3 }, { r: 'za', h: 'ざ', k: 'ザ', g: 'dakuten', l: 3 }, 
+    { r: 'da', h: 'だ', k: 'ダ', g: 'dakuten', l: 3 }, { r: 'ba', h: 'ば', k: 'バ', g: 'dakuten', l: 3 }, 
+    { r: 'pa', h: 'ぱ', k: 'パ', g: 'dakuten', l: 3 }
+];
